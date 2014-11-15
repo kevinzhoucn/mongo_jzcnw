f1b2144c97e347099f7cf0686c0a25758d1d27e8
zzpf <zzpf2007@gmail.com>
2014-11-15 16:11:11 +0800

    avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,6 +15,11 @@ class AvatarUploader < BaseUploader
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # 调整临时文件的存放路径，默认是再 public 下面
+  def cache_dir
+    "/tmp/uploads"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -25,6 +30,13 @@ class AvatarUploader < BaseUploader
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
+
+  process :convert => 'png'
+
+  version :thumb do
+    process :resize_to_fill => [120, 120]
+  end
+
   #
   # def scale(width, height)
   #   # do something
@@ -47,4 +59,31 @@ class AvatarUploader < BaseUploader
   #   "something.jpg" if original_filename
   # end
 
+  # # Override the filename of the uploaded files:
+  # def filename
+  #   if super.present?
+  #     # current_path 是 Carrierwave 上传过程临时创建的一个文件，有时间标记，所以它将是唯一的      
+  #     @name ||= Digest::MD5.hexdigest(current_path)
+  #     "#{@name}.#{file.extension.downcase}"
+  #     # "#{Time.now.strftime("%Y%m%d%H%M%S")}#{rand(1000)}.#{file.extension.downcase}"
+  #   end
+  # end
+
+  # Set the filename for versioned files
+  # def filename
+  #   random_token = Digest::SHA2.hexdigest("#{Time.now.utc}--#{model.id.to_s}").first(20)
+  #   ivar = "@#{mounted_as}_secure_token"    
+  #   token = model.instance_variable_get(ivar)
+  #   token ||= model.instance_variable_set(ivar, random_token)
+  #   "#{model.id}_#{token}.jpg" if original_filename
+  # end
+  def filename
+     "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+    def secure_token(length=16)
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+    end
 end
--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -14,6 +14,8 @@ class BaseUploader < CarrierWave::Uploader::Base
     "photo/#{version_name}.jpg"
   end
 
+  
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
